4d7956956e88232e4d0eb8e15a25fd309c02a08e
Andrew Myhre <andrew.myhre@gmail.com>
2011-08-07 13:35:27 +0100

    calendar year overview and month selection

--- Contractors.Web/Scripts/calendar.js
+++ Contractors.Web/Scripts/calendar.js
@@ -5,51 +5,76 @@ $(document).ready(function () {
     $('#prev').click(function () {
         date = addMonths(date, -1);
         $('#cal').load('/candidates/cal?month=' + (date.getMonth() + 1) + '&year=' + date.getFullYear());
-        showMonth();
+        showMonth(date.getMonth() + 1, date.getFullYear());
         return false;
     });
     $('#next').click(function () {
         date = addMonths(date, 1);
         $('#cal').load('/candidates/cal?month=' + (date.getMonth() + 1) + '&year=' + date.getFullYear());
-        showMonth();
+        showMonth(date.getMonth() + 1, date.getFullYear());
         return false;
     });
+
 
     parseDateFromQueryString(window.location.hash);
 
     dialog = $('<div></div>').dialog({ autoOpen: false, width: 700, resizable: false });
 
-    showMonth();
+    showMonth(date.getMonth() + 1, date.getFullYear());
+
+    $('#months li a').click(function (event) {
+        $('#months li a').removeClass('selected');
+        var month =
+            $(event.target).parentsUntil('ul', 'li').index();
+        showMonth(month + 1, date.getFullYear());
+        $(event.target).addClass('selected');
+        return false;
+    });
 });
+
+function getCountsForYear() {
+    $.get('/candidates/CandidateCountForYear?year=2011',
+        function (response) {
+            var months = $('#months li a .count');
+            for (var i = 0; i < 12; i++)
+                $(months[i]).html('(' + response[i] + ')');
+        });
+}
+
 function parseDateFromQueryString(queryString) {
     if (queryString[0] == '?' || queryString[0] == '#')
         queryString = queryString.substring(1);
     var vars = queryString.split('&');
-    var month = date.getMonth();
+    var month = date.getMonth()+1;
     var year = date.getFullYear();
     for (var i = 0; i < vars.length; i++) {
         var pair = vars[i].split('=');
         if (pair[0] == 'month')
-            month = parseInt(pair[1])-1;
+            month = parseInt(pair[1]);
         else if (pair[0] == 'year')
             year = parseInt(pair[1]);
     }
 
-    date = new Date(year, month, 1);
+    date = new Date(year, month-1, 1);
 }
 
-function showMonth() {
-    $('#cal').load('/candidates/cal',
+function showMonth(month, year) {
+    date = new Date(year, month - 1, 1);
+    dialog.dialog('close');
+    $($('#months li a')[date.getMonth()]).addClass('selected');
+    $('#cal').load('/candidates/cal?month='+month+'&year='+year,
         function () {
             $('.candidate-name').mouseenter(function (event) {
                 var url = '/candidates/Details/' + $(event.target).attr('id') + '?view=tiny';
                 var pos = $(event.target).offset();
                 var width = $(event.target).width();
 
-                dialog.dialog({ title:$(event.target).html(),  position: [pos.left + width, pos.top]});
+                dialog.dialog({ title: $(event.target).html(), position: [pos.left + width, pos.top] });
                 dialog.load(url);
                 dialog.dialog('open');
             });
+
+            getCountsForYear();
         });
 
     var prevMonth = addMonths(date, -1);
@@ -65,7 +90,8 @@ function showMonth() {
             $('#next').html(nextMonth.format('mmmm') + ' (' + response + ')');
         });
 
-        window.location.hash = 'month=' + (date.getMonth()+1) + '&year=' + (date.getFullYear());
+        window.location.hash = 'month=' + month + '&year=' + year;
+        $('#month-heading').html(date.format('mmmm'));
 }
 
 function addMonths(date, monthsToAdd) {
