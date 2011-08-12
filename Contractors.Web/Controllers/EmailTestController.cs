0121f80ae64c701560cdaebd3bb8dc956c762840
Andrew Myhre <andrew@justgiving.com>
2011-08-12 13:05:08 +0100

    integrated email processing

---
+++ Contractors.Web/Controllers/EmailTestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmailProcessing;
+using EmailProcessing.Configuration;
+
+namespace Contractors.Web.Controllers
+{
+    public class EmailTestController : Controller
+    {
+        //
+        // GET: /EmailTest/
+
+        public string Index()
+        {
+            IEmailFacade emailFacade = new EmailFacade(ConfigurationManager.GetSection("email") as EmailBuilderConfigurationSection);
+            emailFacade.LoadTemplates();
+            emailFacade.Send("andrew.myhre@gmail.com", "sample", null);
+
+            return "ok";
+        }
+
+    }
+}
